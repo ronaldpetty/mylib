7c46f4eb7681ff1a2a547f0fa2d9dcf1443b5ee9
Ronald Petty <ronald.petty@rx-m.com>
2020-03-03 12:07:23 -0800

    initial commit

---
+++ cmd/runner.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+import "github.com/ronaldpetty/mylib/pkg/p1"
+
+func main() {
+	fmt.Println(p1.P1())
+}
---
+++ pkg/p2/code.go
@@ -0,0 +1,5 @@
+package p2
+
+func P2() string {
+	return "P2"
+}
